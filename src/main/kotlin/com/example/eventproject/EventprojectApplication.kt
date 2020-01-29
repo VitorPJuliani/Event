2d81f9917fe16fc1d188fce72fad037a95ca4953
Vitor Juliani <vitor.juliani@sympla.com.br>
2020-01-29 11:11:29 -0300

    add project structure

---
+++ src/main/kotlin/com/example/eventproject/EventprojectApplication.kt
@@ -0,0 +1,11 @@
+package com.example.eventproject
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EventprojectApplication
+
+fun main(args: Array<String>) {
+	runApplication<EventprojectApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/eventproject/EventprojectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.eventproject
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EventprojectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
