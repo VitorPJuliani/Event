a636011cd16ac36a5343e12b83ac303505bbf7a2
Vitor Juliani <vitor.juliani@sympla.com.br>
2020-01-29 12:59:04 -0300

    add project models

---
+++ src/main/kotlin/com/example/eventproject/model/Event.kt
@@ -0,0 +1,41 @@
+package com.example.eventproject.model
+
+import java.util.*
+
+data class Event(
+        val id: UUID,
+        val name: String,
+        val description: String,
+        val date: Date,
+        val producer: Producer
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Event
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (description != other.description) return false
+        if (date != other.date) return false
+        if (producer != other.producer) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + name.hashCode()
+        result = 31 * result + description.hashCode()
+        result = 31 * result + date.hashCode()
+        result = 31 * result + producer.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "Event(id=$id, name='$name', description='$description', date=$date, producer=$producer)"
+    }
+
+
+}
