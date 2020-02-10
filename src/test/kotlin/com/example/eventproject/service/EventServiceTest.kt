7e214ef8c914dd71e3bcd5a378849bce88334407
Vitor Juliani <vitor.juliani@sympla.com.br>
2020-02-10 13:43:22 -0300

    event service tests

--- src/test/kotlin/com/example/eventproject/service/EventServiceTest.kt
+++ src/test/kotlin/com/example/eventproject/service/EventServiceTest.kt
@@ -1,6 +1,9 @@
 package com.example.eventproject.service
 
+import com.example.eventproject.exception.ResourceCreateException
 import com.example.eventproject.exception.ResourceNotFoundException
+import com.example.eventproject.exception.ResourceUpdateException
+import com.example.eventproject.form.EventForm
 import com.example.eventproject.model.Event
 import com.example.eventproject.repository.EventRepository
 import io.mockk.every
@@ -8,10 +11,26 @@ import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import java.time.LocalDate
-import java.util.*
+import java.util.UUID
 
 internal class EventServiceTest {
+
+    private val event = Event(
+            id = UUID.randomUUID(),
+            name = "Name",
+            description = "Description",
+            date = LocalDate.parse("2020-02-10"),
+            producer = UUID.randomUUID()
+    )
+
+    private val eventForm = EventForm(
+            name = "Name",
+            description = "Description",
+            date = LocalDate.parse("2020-02-10"),
+            producer = UUID.randomUUID()
+    )
 
     private val repository: EventRepository = mockk()
 
@@ -20,14 +39,6 @@ internal class EventServiceTest {
     @Test
     fun findEventByIdWithCorrectIdShouldReturnEventObject() {
         val uuid = UUID.randomUUID()
-
-        val event = Event(
-                uuid,
-                "Evento teste do vitor",
-                "Espero que esse teste funcione de primeira",
-                LocalDate.parse("2020-02-07"),
-                UUID.fromString("cd37f2ee-c931-4efe-abc3-9050f74324e3")
-        )
 
         every {
             repository.findEventById(uuid)
@@ -47,6 +58,88 @@ internal class EventServiceTest {
         assertThatExceptionOfType(ResourceNotFoundException::class.java)
                 .isThrownBy {
                     service.findEventById(uuid)
+                }
+    }
+
+    @Test
+    fun findAllEventsShouldReturnListOfEvents() {
+        every {
+            repository.findAllEvents()
+        } returns listOf()
+
+        assertThat(service.findAllEvents()).isNotNull
+    }
+
+    @Test
+    fun saveEventWithCorrectBodyShouldNotReturnEventObject() {
+        every {
+            repository.saveEvent(eventForm)
+        } returns event
+
+        assertThat(service.saveEvent(eventForm)).isEqualTo(event)
+    }
+
+    @Test
+    fun saveEventWithNullReturnShouldReturnResourceCreateException() {
+        every {
+            repository.saveEvent(eventForm)
+        } returns null
+
+        assertThatExceptionOfType(ResourceCreateException::class.java)
+                .isThrownBy {
+                    service.saveEvent(eventForm)
+                }
+    }
+
+    @Test
+    fun updateEventWithCorrectBodyShouldReturnEventObject() {
+        val uuid = UUID.randomUUID()
+
+        every {
+            repository.updateEvent(eventForm, uuid)
+        } returns event
+
+        assertThat(service.updateEvent(eventForm, uuid)).isEqualTo(event)
+    }
+
+    @Test
+    fun updateEventWithNullReturnsShouldReturnResourceUpdateException() {
+        val uuid = UUID.randomUUID()
+
+        every {
+            repository.updateEvent(eventForm, uuid)
+        } returns null
+
+        assertThatExceptionOfType(ResourceUpdateException::class.java)
+                .isThrownBy {
+                    service.updateEvent(eventForm, uuid)
+                }
+    }
+
+    @Test
+    fun deleteEventWithCorrectIdShouldNotReturnResourceNotFoundException() {
+        val uuid = UUID.randomUUID()
+
+        every {
+            repository.deleteEvent(uuid)
+        } returns 1
+
+        assertDoesNotThrow {
+            service.deleteEvent(uuid)
+        }
+    }
+
+    @Test
+    fun deleteEventWithIncorrectIdShouldReturnResourceNotFoundException() {
+        val uuid = UUID.randomUUID()
+
+        every {
+            repository.deleteEvent(uuid)
+        } returns 0
+
+        assertThatExceptionOfType(ResourceNotFoundException::class.java)
+                .isThrownBy {
+                    service.deleteEvent(uuid)
                 }
     }
 }
