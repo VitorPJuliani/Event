ba8fd3a49d8d06f6ad243b53f72f4a1ac7052877
Vitor Juliani <vitor.juliani@sympla.com.br>
2020-02-17 17:35:42 -0300

    test container application properties

---
+++ src/test/kotlin/com/example/eventproject/repository/JdbcEventRepositoyTest.kt
@@ -0,0 +1,17 @@
+package com.example.eventproject.repository
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.testcontainers.junit.jupiter.Testcontainers
+
+
+@SpringBootTest
+@Testcontainers
+class JdbcEventRepositoyTest {
+
+    @Test
+    fun test() {
+
+    }
+
+}
